16646f5968804cf77179bf285901856bf13c2bb5
brkhiba <121011109+brkhiba@users.noreply.github.com>
2022-12-30 22:42:01 +0100

    base-to-another-one

--- base vers dicimal.cpp
+++ base vers dicimal.cpp
@@ -6,17 +6,17 @@ int main()
 	/* declaration des variables :
 	    nbr_o = nombre qui affiche par l'utilisateur
 	    nbr_s = nombre de resulta
-	    be = la base d'entree 
+	    bo = la base d'entree 
 	    bs = la base du sortie
 		dic = c'est le nombre en dicimale
 		a et c = c'est des valeur  */
-	int nbr_o , bo , bs , dic , a , c ;
-	int m[100] , i , j , nbr_s , w[100] ; 
+	int bo , bs , dic , a , c ;
+	long nbr_o , nbr_s ; 
 	/* En la deuxieme etap , on va faire l'affctation de la base d'entree et du sortie et le nombre en base d'ontree */
 	printf (" Veulliez donner la base d'ontree : ");
 	scanf ("  %d ", &bo );
 	printf (" Veulliez donner la valeure de nombre en %d : ", bo );
-	scanf (" %d ", & nbr_o );
+	scanf (" %ld ", & nbr_o );
 	printf (" Veulliez donner la base du sortie : ");
 	scanf ("  %d ", &bs );
 	/* on va faire les opperations pour transformer le nombre vers la base decimal */
@@ -31,20 +31,13 @@ int main()
 	printf (" le nombre en dicimal est %d \n ", dic );
 	/* on va faire les opperationts pour transoformer le nombre vers la base de sortie */
     i = 0 ;
-	while ( dic > 0 ){
-		m [ i ] = dic % bs ;
-		dic = dic / bs ;
-		i++ ;
-	}	
-	for ( j = 0 ; j < i ; j++ ){
-		w [ i - j ] = m [ j ] ;
-	}
 	nbr_s = 0 ;
-	a = 1 ;
-	for ( j = 0 ; j < i ; j++ ){
-		nbr_s = nbr_s + w [ j ] * a ;
+	while ( dic != 0 ){
+    	c = dic % bs ;
+		nbr_s = nbr_s + c * a ;
+		dic = dic / bs ;
 		a = a * 10 ;
 	}
-	printf (" le nombre en la base %d est : %d " , bs , nbr_s );
+	printf (" le nombre en la base %d est : %ld " , bs , nbr_s );
 	return 0 ;
 }
